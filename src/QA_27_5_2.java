d8f485980640fbd3035362e5f4d260ad0b3e11b1
MaluLePoulet <168747330+MaluLePoulet@users.noreply.github.com>
2024-05-17 21:06:21 +0400

    Initial commit

---
+++ src/QA_27_5_2.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class QA_27_5_2 {
+    public static void main (String [] args){
+
+        int [] array = new int [10];
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Fill the array with 10 numbers: ");
+        for (int i = 0; i < array.length; i++) {
+            array [i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}

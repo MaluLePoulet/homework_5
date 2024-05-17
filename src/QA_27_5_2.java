9c9259903b7ecbb2472dddb3d2f97b2bcde47fa3
MaluLePoulet <168747330+MaluLePoulet@users.noreply.github.com>
2024-05-17 23:00:41 +0400

    find and print maximum value for the given array

--- src/QA_27_5_2.java
+++ src/QA_27_5_2.java
@@ -2,14 +2,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class QA_27_5_2 {
-    public static void main (String [] args){
+    public static void main(String[] args) {
 
-        int [] array = new int [10];
+        int[] array = new int[10];
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Fill the array with 10 numbers: ");
         for (int i = 0; i < array.length; i++) {
-            array [i] = scanner.nextInt();
+            array[i] = scanner.nextInt();
         }
         System.out.println(Arrays.toString(array));
 
@@ -18,5 +18,13 @@ public class QA_27_5_2 {
             averageValue += (double) array[i] / array.length;
         }
         System.out.println("Average value of the array is: " + averageValue);
+
+        int maxValue = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        System.out.println("Maximum value of the array is: " + maxValue);
     }
 }
